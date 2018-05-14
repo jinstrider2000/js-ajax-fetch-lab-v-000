7435bec74cad7940212b4936fab91a13dc153b85
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-14 16:04:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-"4ad5401121db968f9fd2ae5535f1af49617e9ed6"
-
 function getIssues() {
 }
 
 function showIssues(json) {
+  json
 }
 
 function createIssue() {
+  const repo = 'jinstrider2000/javascript-fetch-lab';
+  fetch(`https://api.github.com/repos/${repo}/issues`,{
+    method: 'POST',
+    headers: {Authorization: `token ${getToken()}`}
+  });
 }
 
 function showResults(json) {
@@ -14,11 +18,15 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  fetch(`https://api.github.com/repos/${repo}/forks`,{
+    method: 'POST',
+    headers: {Authorization: `token ${getToken()}`}
+  });
   //use fetch to fork it!
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return "4ad5401121db968f9fd2ae5535f1af49617e9ed6"
 }
